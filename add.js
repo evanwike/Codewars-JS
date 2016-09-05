0de466fd3b4977c19d4a9c786540cccf97f6893c
Dirtier <sportsdude716@gmail.com>
2016-09-05 15:20:57 -0500

    Chain Adding Function - Codewars Kata

--- add.js
+++ add.js
@@ -1,7 +1,7 @@
 const add = n => { let current = n || 0, fn = function(num){ current += num; return fn }; fn.toString = function(){ return current }; return fn }
 
 
--Test Cases-
-//add(1)// == 1);
+// -Test Cases-
+add(1)// == 1);
 add(1)(2)// == 3);
-//add(1)(2)(3)// == 6);
+add(1)(2)(3)// == 6);
