8ec2d5028d2df0aa1ab7df1f1bbd9145bf342826
Dirtier <sportsdude716@gmail.com>
2016-09-06 21:46:18 -0500

    List Filtering - Codewars Kata

--- filter_list.js
+++ filter_list.js
@@ -1,1 +1,8 @@
 const filter_list = l => { return l.filter( x => { return typeof x != 'string' }) }
+
+
+
+// - Test Cases -
+filter_list([1,2,'a','b'])//,[1,2])
+filter_list([1,'a','b',0,15])//,[1,0,15])
+filter_list([1,2,'aasf','1','123',123])//,[1,2,123])
