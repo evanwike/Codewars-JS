471d20605e35a7bf5b7880b5fda1bccd850e6230
Dirtier <sportsdude716@gmail.com>
2016-09-08 05:43:18 -0500

    Money, Money, Money - Codewars Kata

---
+++ compoundInterest.js
@@ -0,0 +1,20 @@
+function calculateYears(p, i, t, d) {
+  let years = 0;
+  
+  function compound(p, i, t, d) {
+    if (p >= d)
+      return years
+
+    p = (p + (p * i)) - ((p * i) * t);
+    years++;
+    return compound(p, i, t, d)
+  }
+  return compound(p, i, t, d)
+}
+
+
+
+// - Test Cases -
+calculateYears(1000, 0.05, 0.18, 1100)//, 3)
+calculateYears(1000, 0.01625, 0.18, 1200) //, 14)
+calculateYears(1000,0.05,0.18,1000)//, 0)
