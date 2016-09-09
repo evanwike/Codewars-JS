8f192d127fc6c97998efea8ba1738150fe5db753
Evan Wike <sportsdude716@gmail.com>
2016-09-09 02:31:34 -0500

    T.T.T.17: Split Odd and Even - Codewars Kata

--- splitOddAndEven.js
+++ splitOddAndEven.js
@@ -15,3 +15,12 @@ function splitOddAndEven(n){
   })
   return arr.map( x => { return parseInt(x) });
 }
+
+
+// - Test Cases -
+splitOddAndEven(123)  //  [1,2,3])
+splitOddAndEven(223)  //  [22,3])
+splitOddAndEven(111)  //  [111])
+splitOddAndEven(13579)  //  [13579])
+splitOddAndEven(135246)  //  [135,246])
+splitOddAndEven(123456)  //  [1,2,3,4,5,6])
