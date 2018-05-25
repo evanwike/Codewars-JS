a46af7071ce53d395aa4208f53461f8e76e99060
Evan W <sportsdude716@gmail.com>
2018-05-24 22:16:31 -0500

    Update iqTest.js

--- iqTest.js
+++ iqTest.js
@@ -1,14 +1,19 @@
 // IQ Test? God I hope not
 
-function iqTest(numbers){
-  const even = [], odd = [];
-  numbers = numbers.split(' ').map( x => { return parseInt(x) });
-  numbers.map(function(x){
-    if(x % 2 == 0)
-      even.push(numbers.indexOf(x) + 1)
-    else
-      odd.push(numbers.indexOf(x) + 1)
-  })
+const iqTest = numbers => {
+  const even = [], odd = []
+  
+  numbers
+    .split(' ')
+    .forEach(x => {
+      x = Math.trunc(x)
+    
+      if(x % 2 == 0)
+        even.push(numbers.indexOf(x) + 1)
+      else
+        odd.push(numbers.indexOf(x) + 1)
+    })
+  
   return even.length > odd.length ? odd[0] : even[0]
 }
 
